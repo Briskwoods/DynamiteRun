ee464bfbb62804dbb00b3297018b8f8a799db703
Jeff Gichuki <julian.gichuki@gmail.com>
2021-12-08 15:07:52 +0300

    Create CameraController.cs

---
+++ CameraController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private GameObject m_player;
+    [SerializeField] private Camera m_mainCamera;
+
+    [SerializeField] float m_xPos = 0f;
+    [SerializeField] float m_yPos;                              // Height from player
+    [SerializeField] float m_zPos;                              // Distance From Player on Z axis
+
+    // Update is called once per frame
+    void Update()
+    {
+        var playerpos = m_player.transform.position;
+        playerpos.x = m_xPos;
+        m_mainCamera.transform.position = playerpos + new Vector3(m_xPos, m_yPos, -m_zPos);
+    }
+}
